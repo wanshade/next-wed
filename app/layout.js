77681457e6095ff486304c322779ae64d2f5e261
Wawan Darmawan <41707274+wanshade@users.noreply.github.com>
2024-09-13 20:37:46 +0800

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,7 @@
-import localFont from "next/font/local";
+
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+
 
 export const metadata = {
   title: "Wedding Of Karina & Wawan",
@@ -21,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        
       >
         {children}
       </body>
